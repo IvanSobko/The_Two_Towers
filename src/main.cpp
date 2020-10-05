d38fc343bbed2e1d11cd99b48fc84689c899100d
Ivan Sobko <sobko.ivan@gmail.com>
2020-10-05 17:30:24 +0300

    added metric calculations
    Signed-off-by: Ivan Sobko <sobko.ivan@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <map>
 #include <filesystem>
+#include <chrono>
 #include <math.h>
 
 #include "opencv2/core/core.hpp"
@@ -23,8 +24,8 @@ std::vector<std::string> const findSceneImages(const std::string &path) {
     std::vector<std::string> res;
     int i = 0;
     for (const auto &entry : std::filesystem::directory_iterator(path)) {
-        i+=1;
-        if (i%10 != 0) {
+        i += 1;
+        if (i % 3 != 0) {
             continue;
         }
         std::string filename = entry.path().filename().string();
@@ -64,6 +65,7 @@ void processDescriptors(cv::Ptr<cv::xfeatures2d::SURF> detector, cv::Mat image,
     }
 }
 
+
 int main() {
     cv::Mat object = imread("../img/object.jpeg", cv::IMREAD_GRAYSCALE);
 
@@ -84,43 +86,46 @@ int main() {
     if (!size) {
         return 1;
     }
-    //-- Step 1: Detect the keypoints using SURF Detector
+
     int minHessian = 400;
     cv::Ptr<cv::xfeatures2d::SURF> detector = cv::xfeatures2d::SURF::create(minHessian);
-
     cv::Mat objectDescriptor;
     std::vector<cv::Mat> sceneDescriptors;
     sceneDescriptors.resize(size);
-
     std::vector<cv::KeyPoint> objectKeypoints;
     std::vector<std::vector<cv::KeyPoint>> sceneKeypoints;
     sceneKeypoints.resize(size);
-
-    processDescriptors(detector, object, objectKeypoints, objectDescriptor);
-    for (int i = 0; i < size; i++) {
-        processDescriptors(detector, sceneImages[i].second, sceneKeypoints[i], sceneDescriptors[i]);
-        printf("\rDetected and computed %i photos.", i + 1);
-    }
-    printf("\nKeypoints detected.\n");
-
-
-    //-- Step 2: Matching descriptor vectors with a FLANN based matcher
     std::vector<std::pair<std::string, cv::Mat>> readyImg;
     cv::Ptr<cv::FlannBasedMatcher> matcher = cv::FlannBasedMatcher::create();
 
     std::map<std::string, double> pointsInsideAvg;
     std::map<std::string, double> distanceAvg;
+    std::map<std::string, int64_t> duration;
+
+    processDescriptors(detector, object, objectKeypoints, objectDescriptor);
+
+
     for (int i = 0; i < size; i++) {
         const std::string &filename = sceneImages[i].first;
+
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+        //-- Step 1: Detect the keypoints using SURF Detector
+        processDescriptors(detector, sceneImages[i].second, sceneKeypoints[i], sceneDescriptors[i]);
+        printf("\rDetected and computed %i photos.", i + 1);
+
+
         std::vector<std::vector<cv::DMatch>> knn_matches;
 
         if (sceneDescriptors[i].empty()) {
             printf("\nCouldn't find feature points for: %s\n", filename.c_str());
             continue;
         }
+
+        //-- Step 2: Matching descriptor vectors with a FLANN based matcher
         matcher->knnMatch(objectDescriptor, sceneDescriptors[i], knn_matches, 2);
 
-        //-- Filter matches using the Lowe's ratio test
+        //-- Step 3: Filter matches using the Lowe's ratio test
         const float ratio_thresh = 0.7f;
         std::vector<cv::DMatch> good_matches;
         for (const auto &match : knn_matches) {
@@ -129,16 +134,15 @@ int main() {
             }
         }
 
-        //-- Draw matches
+        //-- Step 4: Draw matches
         cv::Mat img_matches;
         drawMatches(object, objectKeypoints, sceneImages[i].second, sceneKeypoints[i],
                     good_matches, img_matches, cv::Scalar::all(-1),
                     cv::Scalar::all(-1), std::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-        //-- Localize the object
+        //-- Step 5: Localize the object
         std::vector<cv::Point2f> obj;
         std::vector<cv::Point2f> scene;
         for (const auto &match : good_matches) {
-            //-- Get the keypoints from the good matches
             obj.push_back(objectKeypoints[match.queryIdx].pt);
             scene.push_back(sceneKeypoints[i][match.trainIdx].pt);
         }
@@ -149,7 +153,7 @@ int main() {
             continue;
         }
 
-        //-- Get the corners from the object.jpeg
+        //-- Step 6: Get the corners from the object.jpeg
         std::vector<cv::Point2f> obj_corners(4);
         obj_corners[0] = cv::Point2f(0, 0);
         obj_corners[1] = cv::Point2f((float) object.cols, 0);
@@ -158,7 +162,8 @@ int main() {
         std::vector<cv::Point2f> scene_corners(4);
 
         perspectiveTransform(obj_corners, scene_corners, H);
-        //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+
+        //-- Step 7: Draw lines between the corners of scene
         line(img_matches, scene_corners[0] + cv::Point2f(object.cols, 0),
              scene_corners[1] + cv::Point2f(object.cols, 0), cv::Scalar(0, 255, 0), 4);
         line(img_matches, scene_corners[1] + cv::Point2f(object.cols, 0),
@@ -168,11 +173,11 @@ int main() {
         line(img_matches, scene_corners[3] + cv::Point2f(object.cols, 0),
              scene_corners[0] + cv::Point2f(object.cols, 0), cv::Scalar(0, 255, 0), 4);
 
-
         printf("\rFile %s is ready [%i/%zu are ready(%i skipped).]", filename.c_str(),
                i + 1, size, (i - static_cast<int>(readyImg.size())));
-        readyImg.push_back({filename, img_matches});
 
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        readyImg.push_back({filename, img_matches});
 
         //time for some metrics
         //first metric
@@ -188,19 +193,21 @@ int main() {
 
         //second metric
         double sumDistance = 0;
-        for(const auto &match: good_matches){
+        for (const auto &match: good_matches) {
             sumDistance += match.distance;
         }
         double distanceRatio = sumDistance / good_matches.size();
         distanceAvg.insert({filename, distanceRatio});
 
+        //third metric
+        auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        duration.insert({filename, time.count()});
     }
 
-    for(const auto &p: distanceAvg){
+    for (const auto &p: distanceAvg) {
         printf("\nFor image %s average point distance: %f", p.first.c_str(), p.second);
     }
     displayImages(readyImg);
-
 
     return 0;
 }
